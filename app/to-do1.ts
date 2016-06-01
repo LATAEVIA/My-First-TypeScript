0ea5d0649f55b63a90a61c12bbff20f242de05f8
LaTaevia Berry <lataevia.berry@gmail.com>
2016-06-01 13:42:46 -0700

    add three child classes HomeTask, HobbyTask, and WorkTask

--- app/to-do1.ts
+++ app/to-do1.ts
@@ -5,11 +5,38 @@ class Task {
     this.done = true;
   }
 }
-class HomeTask extends Task {}
 
 var tasks = [];
-tasks.push(new HomeTask("Do the dishes.", "High"));
-tasks.push(new HomeTask("Buy chocolate.", "Low"));
-tasks.push(new HomeTask("Wash the laundry.", "High"));
+tasks.push(new Task("Do the dishes.", "High"));
+tasks.push(new Task("Buy chocolate.", "Low"));
+tasks.push(new Task("Wash the laundry.", "High"));
 tasks[0].markDone();
 console.log(tasks);
+
+
+
+// ///shortened
+// class Task {
+//   done: boolean;
+//   constructor(public description: string, public priority: string){
+//     this.done = false;
+//   }
+//   markDone(){
+//     this.done = true;
+//   }
+// }
+//
+// ///shortened
+// class Task {
+//   done: boolean;
+//   description: string;
+//   priority: string;
+//   constructor(public description: string, public priority: string){
+//     this.done = false;
+//     this.description = description;
+//     this.priority = priority;
+//   }
+//   markDone(){
+//     this.done = true;
+//   }
+// }
